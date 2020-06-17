0057cfa206dd8c71bacb78a04df85c31729693f9
Screamice <misaelrico786@gmail.com>
2020-06-16 20:39:35 -0500

    Resolviendo problemas

--- Algorithms/Sorting.py
+++ Algorithms/Sorting.py
@@ -6,9 +6,37 @@ class Sorting:
         pivot = randint(1, len(list)-1)
         return pivot
 
-list = [1,2,3,4,5,6,7,8,9]
+    # BUBBLESORT -> O(n²)
+    def Bubblesort(self, list):
+        # Flag para detectar si hubo algún swap
+        flag = True
+
+        while flag:
+            for x in range(len(list)-1):
+                # Condición para hacer un swap.
+                if list[x] > list[x+1]:
+                    list[x], list[x+1] = list[x+1], list[x]
+                    # Se marca false para marcar que hubo un swap.
+                    flag = False
+            # Si hubo un swap se repite el ciclo otra vez para verificar el orden.
+            if(not flag):
+                flag = True
+            # Si no hubo significa que ya todo está ordenado y se sale del ciclo.
+            else:
+                flag = False
+
+        return list
+
+
+    def BucketSort(self, list):
+        bucket = []
+        for x in list:
+            bucket[x] += 1
+
+        return bucket
+
+list = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 static = Sorting()
-pivot = static.Quicksort(list)
-print(pivot)
-print(list[pivot])
+print(static.Bubblesort(list))
+print(static.BucketSort(list))
--- Leetcode/spiral_matrix.py
+++ Leetcode/spiral_matrix.py
@@ -1,3 +1,44 @@
-class Spiral:
-    def __init__(self):
-        self.__alpha = True
+class Solution:
+
+    def spiralOrder(self, matrix):
+        if not matrix: return []
+        # Número de filas y columnas en la matriz.
+        R, C = len(matrix), len(matrix[0])
+
+        # Matriz de tamaño RxC con todos los valores false para saber las posiciones que ya se recorrieron.
+        seen = [[False] * C for _ in matrix]
+
+        #Salida
+        answer = []
+
+        dr = [0, 1, 0, -1]
+        dc = [1, 0, -1, 0]
+
+        # Incio.
+        r = c = di = 0
+
+        for _ in range(R*C):
+            # Se agrega a la salida
+            answer.append(matrix[r][c])
+            # Se marca como vista esa posición.
+            seen[r][c] = True
+
+            cr, cc = r+dr[di], c+dc[di]
+            if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
+                r, c = cr, cc
+            else:
+                di = (di + 1) % 4
+                r, c = r + dr[di], c + dc[di]
+        
+        return answer
+
+
+
+matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+         ]
+
+sol = Solution()
+print(sol.spiralOrder(matrix))
