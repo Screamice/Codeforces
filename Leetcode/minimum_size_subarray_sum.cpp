badfbe66e925f8eb6916b00948b08f3661ba9523
Screamice <misaelrico786@gmail.com>
2020-06-20 23:34:23 -0500

    Resolviendo problemas

--- Leetcode/minimum_size_subarray_sum.cpp
+++ Leetcode/minimum_size_subarray_sum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 class Solution{
     public:
@@ -7,9 +8,25 @@ class Solution{
 };
 
 int Solution::minSubArrayLen(int s, std::vector<int> &nums){
-    for(auto i{0} ; i<nums.size() ; i++){
-        if(s <= nums[i]+nums[i+1]){
-            
+
+    int size = nums.size();
+    if(!size || size == NULL){ return 0; }
+
+    int ans = INT16_MAX, left{0}, sum{0};
+
+    for(auto i=0 ; i<size ; i++){
+        sum += nums[i];
+        while(sum >= s){
+            ans = std::min(ans, i+1-left);
+            sum -= nums[left++];
         }
     }
+
+    return (ans != INT64_MAX)? ans : 0;
+}
+
+int main(){
+    Solution sol;
+    std::vector<int> array = {2,3,1,2,4,3};
+    std::cout << sol.minSubArrayLen(7, array) << std::endl;
 }
